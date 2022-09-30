298d28a141cabdc257080502b55ee2d8d8669c84
Yunus Emre Coskun <yunusemre.coskun@ibtech.com.tr>
2022-09-30 17:27:59 +0300

    Initial Commit

---
+++ TimetoLose/TimetoLoseApp.swift
@@ -0,0 +1,20 @@
+//
+//  TimetoLoseApp.swift
+//  TimetoLose
+//
+//  Created by T66140 on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimetoLoseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
