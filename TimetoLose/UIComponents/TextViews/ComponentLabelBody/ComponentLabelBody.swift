3118aafd1d804fcf21bcd869ce0805c2786fb1d9
Ulas Goktas <u.ulas.goktas@gmail.com>
2022-11-28 15:33:23 +0300

    Update ComponentLabels multiline text alignment

--- TimetoLose/UIComponents/TextViews/ComponentLabelBody/ComponentLabelBody.swift
+++ TimetoLose/UIComponents/TextViews/ComponentLabelBody/ComponentLabelBody.swift
@@ -11,10 +11,12 @@ struct ComponentLabelBody: View, BaseLabelProtocol {
     var text: String
     var font: Font = .bodyPrimary
     var textColor: Color = .gray
+    var textAlignment: TextAlignment = .center
 
     var body: some View {
         Text(text)
             .font(font)
             .foregroundColor(textColor)
+            .multilineTextAlignment(textAlignment)
     }
 }
