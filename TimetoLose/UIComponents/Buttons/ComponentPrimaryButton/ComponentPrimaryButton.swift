e0ecf2bb48362b3fcb7afe0c3d323acdc00a98c4
Ulas Goktas <u.ulas.goktas@gmail.com>
2022-11-29 18:51:29 +0300

    Add leftImage property to ComponentPrimaryButton

--- TimetoLose/UIComponents/Buttons/ComponentPrimaryButton/ComponentPrimaryButton.swift
+++ TimetoLose/UIComponents/Buttons/ComponentPrimaryButton/ComponentPrimaryButton.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ComponentPrimaryButton: View {
     let title: String
     var font: Font = .bodyRegular
+    var showLeftImage = false
+    var leftImageURL = ""
     var titleColor: Color = .white
     var horizontalPadding: CGFloat = .regularPoint
     var verticalPadding: CGFloat = .regularPoint
@@ -24,14 +26,27 @@ struct ComponentPrimaryButton: View {
 
     var body: some View {
         Button(action: action, label: {
-            Text(title)
-                .frame(width: width, height: height, alignment: titleAlignment)
-                .font(font)
-                .padding(.horizontal, horizontalPadding)
-                .padding(.vertical, verticalPadding)
-                .foregroundColor(titleColor)
+            HStack {
+                if showLeftImage {
+                    createImageView(imageURL: leftImageURL)
+                }
+                Text(title)
+                    .frame(width: width, height: height, alignment: titleAlignment)
+                    .font(font)
+                    .padding(.horizontal, horizontalPadding)
+                    .padding(.vertical, verticalPadding)
+                    .foregroundColor(titleColor)
+            }
         })
             .background(isDisabled.wrappedValue ? disabledColor : buttonColor)
             .cornerRadius(cornerRadius)
+    }
+    @ViewBuilder
+    private func createImageView(imageURL: String) -> some View {
+        Image(imageURL)
+            .resizable()
+            .frame(width: .doubleRegularPoint, height: .doubleRegularPoint)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, 0)
     }
 }
