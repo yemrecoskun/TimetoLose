b69aef2ffe0d25a3336125d8c830f71b1187298f
Ulas Goktas <u.ulas.goktas@gmail.com>
2022-11-29 19:57:19 +0300

    Add AnyDiseaseView

--- TimetoLose/Utilities/AppText.swift
+++ TimetoLose/Utilities/AppText.swift
@@ -19,7 +19,7 @@ struct AppText {
 
     struct GoalView {
         static let header = "What's your goal?"
-        static let body = "We'll personalize recommendations based on your goals"
+        static let body = "We'll personalize recommendations based on your goals."
         static let firstChoice = "Healtier life"
         static let secondChoice = "Boost my energy"
         static let thirdChoice = "Stay motivated"
@@ -28,14 +28,14 @@ struct AppText {
 
     struct GenderView {
         static let header = "What's your gender?"
-        static let body = "We need to know your gender to accurately calculate your daily calorie goal"
+        static let body = "We need to know your gender to accurately calculate your daily calorie goal."
         static let female = "Female"
         static let male = "Male"
     }
 
     struct ActivityLevelView {
         static let header = "How active are you?"
-        static let body = "Knowing your daily activity level helps us calculate your calorie need more accurately"
+        static let body = "Knowing your daily activity level helps us calculate your calorie need more accurately."
         static let firstActivityLevelTitle = "Lightly active"
         static let firstActivityLevelHint = "Mostly sitting, e.g office worker"
         static let secondActivityLevelTitle = "Moderately active"
@@ -48,7 +48,7 @@ struct AppText {
 
     struct SpecificDietView {
         static let header = "Do you follow a specific diet?"
-        static let body = "We'll adapt our recommendations to your preferences"
+        static let body = "We'll adapt our recommendations to your preferences."
         static let firstDietTitle = "Classic"
         static let firstDietHint = "I have no dietary preferences"
         static let secondDietTitle = "Pescaterian"
@@ -61,14 +61,29 @@ struct AppText {
 
     struct AnySnackView {
         static let header = "Do you have any snack?"
-        static let body = "We'll personalize recommendations based on your eating habits"
+        static let body = "We'll personalize recommendations based on your eating habits."
         static let no = "No"
         static let one = "1"
         static let two = "2"
         static let three = "3"
         static let four = "4"
     }
-    
+
+    struct AnyDiseaseView {
+        static let header = "Do you have any disease?"
+        static let body = "We'll personalize recommendation based on your health condition."
+        static let noDisease = "No disease"
+        static let diabetes = "Diabetes"
+        static let hypertension = "Hypertension"
+        static let hypotension = "Hypotension"
+        static let nephropathy = "Nephropathy"
+        static let liver = "Liver"
+        static let cancer = "Cancer"
+        static let autoImmune = "Auto-Immune"
+        static let metabolic = "Metabolic"
+        static let other = "Other"
+    }
+
     static func getText(_ text: Text) -> String {
         return text.rawValue
     }
